8f0ca7654a910a11b424ea70b04404df021c51b3
Nacho Jacquot <168210578+nacjacds@users.noreply.github.com>
2024-07-07 07:37:54 +0200

    update app

--- app/telechurn_app.py
+++ app/telechurn_app.py
@@ -1,17 +1,31 @@
-import streamlit as st
-import pickle
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from xgboost import XGBClassifier
+from sklearn.ensemble import StackingClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+from sklearn.preprocessing import StandardScaler
 
 # Cargar el modelo
-model = pickle.load(open('modelo.pkl', 'rb'))
+with open('models/modelo_gradientboosting.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
 
 # Título de la aplicación
-st.title("Mi Aplicación de Predicción")
-
-# Entrada de datos del usuario
-input_data = st.text_input("Introduce los datos de entrada (separados por comas):")
-
-if st.button("Predecir"):
-    input_data = np.array([float(i) for i in input_data.split(',')]).reshape(1, -1)
-    prediction = model.predict(input_data)
-    st.write(f"La predicción es: {prediction}")
+st.title("Churn Prediction App")
