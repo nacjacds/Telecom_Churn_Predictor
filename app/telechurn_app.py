33ded6b4d192cf731bc381268dbe62ffe0f55c04
Nacho Jacquot <168210578+nacjacds@users.noreply.github.com>
2024-07-07 19:53:45 +0200

    upgrade

--- app/telechurn_app.py
+++ app/telechurn_app.py
@@ -21,9 +21,10 @@ import streamlit as st
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
+
 # Cargar el modelo
 try:
-    with open('models/modelo_gradientboosting.pkl', 'rb') as f:
+    with open('/mnt/data/modelo_gradientboosting.pkl', 'rb') as f:
         loaded_model = pickle.load(f)
 except FileNotFoundError:
     st.error("El archivo del modelo no se encuentra en la ruta especificada.")
@@ -33,9 +34,9 @@ except Exception as e:
     st.stop()
 
 # Definir las columnas categóricas y numéricas
-categorical_features = ['gender', 'SeniorCitizen', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines', 
-                        'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 
-                        'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod']
+categorical_features = ['Contract', 'InternetService', 'PaymentMethod', 'OnlineSecurity', 'OnlineBackup',
+                        'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'MultipleLines',
+                        'Dependents', 'Partner']
 numeric_features = ['tenure', 'MonthlyCharges', 'TotalCharges']
 
 # Crear el transformador de columnas
@@ -47,22 +48,18 @@ preprocessor = ColumnTransformer(
 
 # Crear un DataFrame de ejemplo para ajustar el codificador
 dummy_data = pd.DataFrame({
-    'gender': ['Male'],
-    'SeniorCitizen': [0],
-    'Partner': ['Yes'],
-    'Dependents': ['No'],
-    'PhoneService': ['Yes'],
-    'MultipleLines': ['No phone service'],
-    'InternetService': ['DSL'],
-    'OnlineSecurity': ['No internet service'],
-    'OnlineBackup': ['No internet service'],
-    'DeviceProtection': ['No internet service'],
-    'TechSupport': ['No internet service'],
-    'StreamingTV': ['No internet service'],
-    'StreamingMovies': ['No internet service'],
     'Contract': ['Month-to-month'],
-    'PaperlessBilling': ['Yes'],
+    'InternetService': ['DSL'],
     'PaymentMethod': ['Electronic check'],
+    'OnlineSecurity': ['Yes'],
+    'OnlineBackup': ['Yes'],
+    'DeviceProtection': ['Yes'],
+    'TechSupport': ['Yes'],
+    'StreamingTV': ['Yes'],
+    'StreamingMovies': ['Yes'],
+    'MultipleLines': ['No phone service'],
+    'Dependents': ['No'],
+    'Partner': ['Yes'],
     'tenure': [1],
     'MonthlyCharges': [29.85],
     'TotalCharges': [29.85]
@@ -90,47 +87,39 @@ def predict_churn(data):
 st.write("Introduce las características del cliente:")
 
 # Entradas de las características
-gender = st.selectbox('Gender', ['Male', 'Female'])
-SeniorCitizen = st.selectbox('Senior Citizen', [0, 1])
-Partner = st.selectbox('Partner', ['Yes', 'No'])
-Dependents = st.selectbox('Dependents', ['Yes', 'No'])
-PhoneService = st.selectbox('Phone Service', ['Yes', 'No'])
-MultipleLines = st.selectbox('Multiple Lines', ['Yes', 'No', 'No phone service'])
-InternetService = st.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'])
-OnlineSecurity = st.selectbox('Online Security', ['Yes', 'No', 'No internet service'])
-OnlineBackup = st.selectbox('Online Backup', ['Yes', 'No', 'No internet service'])
-DeviceProtection = st.selectbox('Device Protection', ['Yes', 'No', 'No internet service'])
-TechSupport = st.selectbox('Tech Support', ['Yes', 'No', 'No internet service'])
-StreamingTV = st.selectbox('Streaming TV', ['Yes', 'No', 'No internet service'])
-StreamingMovies = st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet service'])
-Contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
-PaperlessBilling = st.selectbox('Paperless Billing', ['Yes', 'No'])
-PaymentMethod = st.selectbox('Payment Method', ['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'])
-tenure = st.slider('Tenure (months)', 1, 72, 1)
-MonthlyCharges = st.number_input('Monthly Charges', min_value=0.0, max_value=150.0, value=70.0)
-TotalCharges = st.number_input('Total Charges', min_value=0.0, max_value=10000.0, value=70.0)
+contract = st.selectbox('Contract', ['Month-to-month', 'One year', 'Two year'])
+internet_service = st.selectbox('Internet Service', ['DSL', 'Fiber optic', 'No'])
+payment_method = st.selectbox('Payment Method', ['Electronic check', 'Mailed check', 'Bank transfer (automatic)', 'Credit card (automatic)'])
+online_security = st.selectbox('Online Security', ['Yes', 'No', 'No internet service'])
+online_backup = st.selectbox('Online Backup', ['Yes', 'No', 'No internet service'])
+device_protection = st.selectbox('Device Protection', ['Yes', 'No', 'No internet service'])
+tech_support = st.selectbox('Tech Support', ['Yes', 'No', 'No internet service'])
+streaming_tv = st.selectbox('Streaming TV', ['Yes', 'No', 'No internet service'])
+streaming_movies = st.selectbox('Streaming Movies', ['Yes', 'No', 'No internet service'])
+multiple_lines = st.selectbox('Multiple Lines', ['Yes', 'No', 'No phone service'])
+dependents = st.selectbox('Dependents', ['Yes', 'No'])
+partner = st.selectbox('Partner', ['Yes', 'No'])
+tenure = st.slider('Tenure (months)', 0, 72, 1)
+monthly_charges = st.number_input('Monthly Charges', min_value=0.0, max_value=150.0, value=70.0)
+total_charges = st.number_input('Total Charges', min_value=0.0, max_value=10000.0, value=70.0)
 
 # Convertir las entradas en un formato adecuado para el modelo
 input_data = pd.DataFrame({
-    'gender': [gender],
-    'SeniorCitizen': [SeniorCitizen],
-    'Partner': [Partner],
-    'Dependents': [Dependents],
-    'PhoneService': [PhoneService],
-    'MultipleLines': [MultipleLines],
-    'InternetService': [InternetService],
-    'OnlineSecurity': [OnlineSecurity],
-    'OnlineBackup': [OnlineBackup],
-    'DeviceProtection': [DeviceProtection],
-    'TechSupport': [TechSupport],
-    'StreamingTV': [StreamingTV],
-    'StreamingMovies': [StreamingMovies],
-    'Contract': [Contract],
-    'PaperlessBilling': [PaperlessBilling],
-    'PaymentMethod': [PaymentMethod],
+    'Contract': [contract],
+    'InternetService': [internet_service],
+    'PaymentMethod': [payment_method],
+    'OnlineSecurity': [online_security],
+    'OnlineBackup': [online_backup],
+    'DeviceProtection': [device_protection],
+    'TechSupport': [tech_support],
+    'StreamingTV': [streaming_tv],
+    'StreamingMovies': [streaming_movies],
+    'MultipleLines': [multiple_lines],
+    'Dependents': [dependents],
+    'Partner': [partner],
     'tenure': [tenure],
-    'MonthlyCharges': [MonthlyCharges],
-    'TotalCharges': [TotalCharges]
+    'MonthlyCharges': [monthly_charges],
+    'TotalCharges': [total_charges]
 })
 
 # Botón para predecir
